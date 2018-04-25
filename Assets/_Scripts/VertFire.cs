cdacf873f1a27765a93e40a7ba57f3805f566326
Mike Riley <rileyma@rose-hulman.edu>
2018-04-25 14:44:50 -0400

    pushing work

---
+++ Assets/_Scripts/VertFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertFire : MonoBehaviour {
+
+    private Animator animatorComp = null;
+
+	// Use this for initialization
+	void Start () {
+        animatorComp = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animatorComp.SetTrigger("FireVert");
+        }
+		
+	}
+}
