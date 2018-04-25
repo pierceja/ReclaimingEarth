47f1f375879eafc3e513d01ade96a37433705c87
wlucas7474 <weierlj@rose-hulman.edu>
2018-04-25 12:33:02 -0400

    Triggers work in progress. Not very necessary tho, so not doing

---
+++ Assets/Objective.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour {
+
+    static public bool objectiveChanged = false;
+    static public string objectiveName = "none";
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            objectiveName = name;
+            objectiveChanged = true;
+            gameObject.active = false;
+        }
+    }
+}
---
+++ Assets/ObjectiveHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveHandler : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public GameObject greenCheckmark;
+    public GameObject redCheckmark;
+    public GameObject yellowCheckmark;
+    public GameObject blueCheckmark;
+    public GameObject[] objectives;
+
+    void Update()
+    {
+        // Check for completed objectives
+        if (Objective.objectiveChanged)
+        {
+
+            Objective.objectiveChanged = false;
+        }
+    }
+}
