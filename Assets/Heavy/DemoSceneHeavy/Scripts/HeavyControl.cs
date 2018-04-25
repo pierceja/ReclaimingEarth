b6341bf9d2b4c21b84203f74fa704d2b1277f6b8
nkghjhkgj <max1@rose-hulman.edu>
2018-04-26 00:19:00 +0800

    4/26
    alien model and FSMs

---
+++ Assets/Heavy/DemoSceneHeavy/Scripts/HeavyControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyController : MonoBehaviour {
+	public Animator animator;
+	public float time = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Walk ();
+		Roar ();
+	}
+
+	public void Idle ()
+	{
+		animator = GetComponent<Animator>();
+		animator.SetBool ("Walk", false);
+		animator.SetBool ("SprintJump", false);
+		animator.SetBool ("PlayerFound", false);
+	
+
+	}
+
+	public void Walk ()
+	{
+		animator = GetComponent<Animator>();
+		animator.SetBool ("Walk", true);
+		animator.SetBool ("SprintJump", false);
+		animator.SetBool ("PlayerFound", false);
+		animator.SetFloat ("TimePassed", time);
+
+	}
+
+	public void Roar()
+	{
+		animator = GetComponent<Animator>();
+		animator.SetBool ("Walk", false);
+		animator.SetBool ("SprintJump", true);
+		animator.SetBool ("PlayerFound", false);
+		animator.SetFloat ("TimePassed", time);
+
+	}
+
+	public void LightMachineGun()
+	{
+		animator = GetComponent<Animator>();
+		animator.SetFloat ("TimePassed", time);
+
+	}
+}
