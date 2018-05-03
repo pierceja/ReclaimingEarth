58598df1d9a3f27fb9f4407b933cebc53962eb3c
wlucas7474 <weierlj@rose-hulman.edu>
2018-05-03 15:38:59 -0400

    Objectives UI updates correctly

--- Assets/ObjectiveHandler.cs
+++ Assets/ObjectiveHandler.cs
@@ -1,23 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveHandler : MonoBehaviour {
 
     [Header("Set in Inspector")]
-    public GameObject greenCheckmark;
-    public GameObject redCheckmark;
-    public GameObject yellowCheckmark;
-    public GameObject blueCheckmark;
-    public GameObject[] objectives;
+    public Toggle greenCheckmark;
+    public Toggle redCheckmark;
+    public Toggle yellowCheckmark;
+    public Toggle blueCheckmark;
 
     void Update()
     {
         // Check for completed objectives
         if (Objective.objectiveChanged)
         {
-
+            ToggleCheckmark(Objective.objectiveName);
+            Objective.objectiveName = "none";
             Objective.objectiveChanged = false;
         }
+    }
+
+    void ToggleCheckmark(string name)
+    {
+        switch (name)
+        {
+            case "GreenCylinder":
+                greenCheckmark.isOn = true;
+                break;
+            case "RedCylinder":
+                redCheckmark.isOn = true;
+                break;
+            case "YellowCylinder":
+                yellowCheckmark.isOn = true;
+                break;
+            case "BlueCylinder":
+                blueCheckmark.isOn = false;
+                break;
+            default:
+                print("Error, name doesn't match any cylinder colors");
+                break;
+        }
+
     }
 }
