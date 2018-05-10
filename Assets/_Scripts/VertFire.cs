5b5b63ea04fb8a8b0e300987af6341cd393578c0
Mike Riley <rileyma@rose-hulman.edu>
2018-05-10 15:20:27 -0400

    gun on player prefab

--- Assets/_Scripts/VertFire.cs
+++ Assets/_Scripts/VertFire.cs
@@ -7,7 +7,9 @@ public class VertFire : MonoBehaviour {
     private Animator animatorComp = null;
     private AudioSource fire;
     private AudioSource reload;
+    private AudioSource dryFire;
     private AudioSource[] sounds;
+    private int clipSize = 30;
 
 	// Use this for initialization
 	void Start () {
@@ -15,19 +17,32 @@ public class VertFire : MonoBehaviour {
         sounds = GetComponents<AudioSource>();
         fire = sounds[0];
         reload = sounds[1];
+        dryFire = sounds[2];
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            fire.Play();
-            animatorComp.SetTrigger("FireVert");
+            if (clipSize > 0)
+            {
+                fire.Play();
+                animatorComp.SetTrigger("FireVert");
+                clipSize--;
+            }
+            else
+            {
+                dryFire.Play();
+            }
+            
         } 
         else if (Input.GetKeyDown(KeyCode.R))
         {
             reload.Play();
             animatorComp.SetTrigger("ReloadVert");
+            // need to wait two seconds before executing next line
+            // can't fire while reloading
+            clipSize = 30;
         }
 	}
 }
