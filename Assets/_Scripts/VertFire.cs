c6d22fe884cc6f2f4c8ac7fe6999ba53cd0e036f
Mike Riley <rileyma@rose-hulman.edu>
2018-05-10 13:13:02 -0400

    fixed sounds

--- Assets/_Scripts/VertFire.cs
+++ Assets/_Scripts/VertFire.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class VertFire : MonoBehaviour {
 
     private Animator animatorComp = null;
+    private AudioSource fire;
+    private AudioSource reload;
+    private AudioSource[] sounds;
 
 	// Use this for initialization
 	void Start () {
         animatorComp = GetComponent<Animator>();
+        sounds = GetComponents<AudioSource>();
+        fire = sounds[0];
+        reload = sounds[1];
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            // add a sound here if wanted
-            AudioSource fire = GetComponent<AudioSource>();
             fire.Play();
             animatorComp.SetTrigger("FireVert");
         } 
         else if (Input.GetKeyDown(KeyCode.R))
         {
-            AudioSource reload = GetComponent<AudioSource>();
             reload.Play();
             animatorComp.SetTrigger("ReloadVert");
         }
-		
 	}
 }
